e0e3ca2936745bd7d89421311a685d7f1ccb8156
Lee Young Seo <lupin204@gmail.com>
2018-07-02 17:52:57 +0900

    validate tx..

--- src/transaction.js
+++ src/transaction.js
@@ -140,20 +140,53 @@ const updateUTxOuts = (newTxs, uTxOutList) => {
 // check validation of tx.
 const isTxInStructureValid = (txIn) => {
     if (txIn === null) {
+        console.log("The txIn appears to be null");
         return false;
     } else if (typeof txIn.signature !== 'string') {
+        console.log("The txIn doesn't have a valid signature");
         return false;
     } else if (typeof txIn.txOutId !== 'string') {      // hash
+        console.log("The txIn doesn't have a valid txOutId");
         return false;
     } else if (typeof txIn.txOutIndex !== 'number') {
+        console.log("The txIn doesn't have a valid txOutIndex");
         return false;
     } else {
         return true;
     }
 }
+
+// address 검증 : address(public key) 길이는 130자
+const isAddressValid = (address) => {
+    // check address length is 130
+    if (adderss.length !== 130) {
+        return false;
+    // check address is hexa-decimal(16진수)
+    } else if (address.match("^[a-fA-F0-9]+$") === null) {
+        return false;
+    } else if (!address.startsWith('04')) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 const isTxOutStructureValid = (txOut) => {
-    // to-do
-    return true;
+    if (txOut === null) {
+        return false;
+    } else if (typeof txOut.address !== 'string') {
+        console.log("The txOut doesn't have a valid string as address");
+        return false;
+    } else if (!isAddressValid(txOut.address)) {
+        console.log("The txOut doesn't have a valid address");
+        return false;
+    } else if (typeof txOut.amount !== 'number') {
+        console.log("The txOut doesn't have a valid amount");
+        return false;
+    } else {
+        return true;
+    }
+    
 }
 const isTxStructureValid = (tx) => {
     if (typeof tx.id !== 'string') {
@@ -162,13 +195,17 @@ const isTxStructureValid = (tx) => {
     } else if (!(tx.txIns instanceof Array)) {
         console.log('The TxIns are not an array');
         return false;
-    } else if (!isTxInStructureValid(tx.txIns)) {
+    // txIn_array를 map을 돌려서 각각의 txIn을 isTxInStructureValid해서 모든 결과값이 true가 나오는지 체크
+    // [true, true, false].reduce((a, b) => a && b, true) = false   // false인 결과값이 1개 있음 -> 최종 리턴은 false
+    // [true, true, true].reduce((a, b) => a && b, true) = true     // 모든 결과값이 true -> 최종 리턴은 true
+    } else if (!tx.txIns.map(isTxInStructureValid).reduce((a, b) => a && b, true)) {
         console.log('The structure of one of the txIn is not valid');
         return false;
     } else if (!(tx.txOuts instanceof Array)) {
         console.log('The TxOuts are not an array');
         return false;
-    } else if (!isTxOutStructureValid(tx.txOuts)) {
+    // txOut_array를 map을 돌려서 각각의 txOut을 isTxOutStructureValid해서 모든 결과값이 true가 나오는지 체크
+    } else if (!tx.txOuts.map(isTxOutStructureValid).reduce((a, b) => a && b, true)) {
         console.log('The structure of one of the txOut is not valid');
     } else {
         return true;
